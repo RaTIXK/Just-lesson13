7376e8d0c020e8f44ae6bdbb05603e4c05412a94
RaTIXK <ratikvariani@gmail.com>
2023-04-13 12:53:10 +0300

    Commit ObserverPatern V 1.0

---
+++ src/com/project13_observer/Cxeni.java
@@ -0,0 +1,8 @@
+package com.project13_observer;
+
+public class Cxeni extends Observer {
+    @Override
+    public void update(Object... args) {
+        String title = (String) args [0];
+    }
+}
--- src/com/project13_observer/Youtube.java
+++ src/com/project13_observer/Youtube.java
@@ -1,4 +1,23 @@
 package com.project13_observer;
+import com.project13_observer.Subject;
 
-public class Youtube {
+public class Youtube extends Subject{
+    private String title = "YouTube.com";
+
+    public Youtube() {
+    }
+
+    public Youtube(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notify(title);
+    }
+
 }
