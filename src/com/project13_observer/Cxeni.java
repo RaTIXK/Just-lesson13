6092f1c271eaff0d3cfb1dd13aee6d79b3cbcf34
RaTIXK <ratikvariani@gmail.com>
2023-04-13 13:07:56 +0300

    Commit ObserverPatern V 1.0

--- src/com/project13_observer/Cxeni.java
+++ src/com/project13_observer/Cxeni.java
@@ -4,5 +4,6 @@ public class Cxeni extends Observer {
     @Override
     public void update(Object... args) {
         String title = (String) args [0];
+        System.out.println("I noticed that youtube changed title to: " + title);
     }
 }
